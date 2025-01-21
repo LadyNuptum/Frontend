657c5a840df49d19210d403a144e0219f9783957
OscarTorresDev <torresoscar.iq@gmail.com>
2025-01-20 22:13:44 -0500

    actualizacion 3 nav bar , banner y redireccionamiento rutas 2

--- JS/aboutUs.js
+++ JS/aboutUs.js
@@ -1,21 +1,21 @@
- // Obtener el título de la página
- const pageTitle = document.title;
+// Obtener el título de la página
+const pageTitle = document.title;
 
- // Insertar el título en el navbar
- const navbarTitle = document.getElementById('navbar-page-title');
- navbarTitle.textContent = pageTitle;
+// Insertar el título en el navbar
+const navbarTitle = document.getElementById("navbar-page-title");
+navbarTitle.textContent = pageTitle;
 
- //comentarios oscar 
- // mostrar y ocultar el fondo sticky y el título
- window.onscroll = function () { //onscroll sigue le scroll para una accion determinada
-   const navbar = document.querySelector('header');//selecciona el header
-   const titleContainer = document.querySelector('.navbar-title');//selecciona mi titulo del header
-   
-   if (window.scrollY > 10) {//cuando scrol es mayor a 0 se activa
-     navbar.classList.add('sticky-navbar'); // Este ctiva el fondo cristal
-     titleContainer.classList.remove('d-none'); // Asegura que el título esté visible
-   } else {
-     navbar.classList.remove('sticky-navbar'); // Desactivar fondo cristal
-     titleContainer.classList.add('d-none'); // oculta el titulo cuando está en la parte superior
-   }
- };
+// mostrar y ocultar el fondo sticky y el título
+window.onscroll = function () {
+  
+  const navbar = document.querySelector("header");
+  const titleContainer = document.querySelector(".navbar-title");
+
+  if (window.scrollY > 10) {
+    navbar.classList.add("sticky-navbar");
+    titleContainer.classList.remove("d-none");
+  } else {
+    navbar.classList.remove("sticky-navbar");
+    titleContainer.classList.add("d-none");
+  }
+};
