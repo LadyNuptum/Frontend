c2267a0f17be9fa84b79a8778aefec04650b6cb7
OscarTorresDev <torresoscar.iq@gmail.com>
2025-01-30 10:41:41 -0500

    actualizacion tarea 9

--- JS/singUp.js
+++ JS/singUp.js
@@ -1,35 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector(".form-register");
-  const btnSubmit = document.querySelector(".btn-regiter");
+  const btnSubmit = document.querySelector(".btn-register");
   const aceptTerms = document.getElementById("acept");
   const inputs = form.querySelectorAll("input:not([type='submit'])");
 
-  // Desactivar el botun
+  // desactiva btn
   btnSubmit.disabled = true;
 
-  // Patrones
+  // patrones validación
   const patterns = {
     name: /^[A-Za-zÁáÉéÍíÓóÚúÑñ\s]{2,30}$/,
     lastName: /^[A-Za-zÁáÉéÍíÓóÚúÑñ\s]{2,30}$/,
     email: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
     phone: /^[0-9]{10}$/,
-    password: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+    password: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/,
   };
 
-  // Mensajeserror
+  // nmnsajeserror
   const errorMessages = {
-    name: "El nombre no puede estar vacío y debe contener solo letras (2-30 caracteres).",
+    name: "El nombre debe contener solo letras (2-30 caracteres).",
     lastName: "El apellido debe tener entre 2 y 30 caracteres y solo letras.",
     email: "Ingrese un correo electrónico válido.",
     phone: "El teléfono debe tener 10 dígitos numéricos.",
     password:
-      "La contraseña debe tener al menos 8 caracteres, una letra y un número.",
+      "La contraseña debe tener al menos 6 caracteres, una letra y un número.",
     "confirm-password": "Las contraseñas no coinciden.",
     acept: "Debe aceptar los términos y condiciones.",
     empty: "Este campo no puede estar vacío.",
   };
 
-  // mostrar mensaje error
+  // mensaje de error
   function showError(input, message) {
     clearError(input);
     const errorDiv = document.createElement("div");
@@ -42,19 +42,19 @@ document.addEventListener("DOMContentLoaded", () => {
     input.parentElement.appendChild(errorDiv);
   }
 
-  // limpiar errores
+  // clear errores
   function clearError(input) {
     const errorDiv = input.parentElement.querySelector(".error-message");
     if (errorDiv) errorDiv.remove();
-    input.style.border = "1px solid #ccc";
+    input.style.border = "3px solid #27d604";
   }
 
-  // alidación campos
+  // validar campos
   function validateField(input) {
     const inputType = input.id;
     const value = input.value.trim();
 
-    //estos son para los vacíos
+    //campo está vacío
     if (!value) {
       showError(
         input,
@@ -63,39 +63,43 @@ document.addEventListener("DOMContentLoaded", () => {
       return false;
     }
 
-    // este para termino y condicion
+    // checkbox terminos
     if (inputType === "acept") {
       if (!input.checked) {
         showError(input, errorMessages[inputType]);
         return false;
       }
     }
-    // confirmación de contraseña
+    // confirma contraseña
     else if (inputType === "confirm-password") {
       if (value !== document.getElementById("password").value) {
         showError(input, errorMessages[inputType]);
         return false;
       }
     }
-    // Validar otros campos con patrones
+    // patrones definidos
+    else if (patterns[inputType] && !patterns[inputType].test(value)) {
+      showError(input, errorMessages[inputType]);
+      return false;
+    }
 
     clearError(input);
     return true;
   }
 
-  // Habilitaboton enviar cuando usuario acepta
+  // Habilitar botón de enviar cuando usuario acepte términos
   aceptTerms.addEventListener("change", () => {
     btnSubmit.disabled = !aceptTerms.checked;
   });
 
-  // Validación reraltime
+  // alidación tiempo real
   inputs.forEach((input) => {
     input.addEventListener("input", () => {
       validateField(input);
     });
   });
 
-  //envío del formulario
+  // envio del form
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
@@ -110,7 +114,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     if (isValid) {
-      //objeto Jsno datos usuario
+      // objeto Josn con datos del usuario
       const userData = {};
       inputs.forEach((input) => {
         if (input.id !== "confirm-password") {
@@ -119,25 +123,29 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       });
 
-      // Metodo para encriptar contraseña
+      // encriptacion contraseña
       const encryptedPassword = await hashPassword(userData.password);
       userData.password = encryptedPassword;
 
-      //  usuarios existentes y array vacio
+      // save en localStorage
       const existingUsers = JSON.parse(localStorage.getItem("users")) || [];
       existingUsers.push(userData);
       localStorage.setItem("users", JSON.stringify(existingUsers));
+
+      // mensaje de éxito
       const messageContainer = document.querySelector("#success-message");
-      messageContainer.innerHTML = `<p style="color: green; font-weight: bold;">Registro exitoso.</p>`;
+      messageContainer.innerHTML = `<p>Registro exitoso ✔</p>`;
       btnSubmit.disabled = true;
+
+      
       setTimeout(() => {
         form.reset();
         messageContainer.innerHTML = "";
-      }, 3000); //milisegundo
+      }, 3000);
     }
   });
 
-  // Función para encriptar la contraseña usando SHA-256
+  //encriptar la contraseña usando SHA-256
   async function hashPassword(password) {
     const encoder = new TextEncoder();
     const data = encoder.encode(password);
